2c14b1beb6b926dc2b1edbde445db5b08e5401ce
Rocha@CodSec <nuno.rocha@codsec.io>
2023-10-04 17:28:40 +0100

    chore(bootstrap) Bootstrap project

---
+++ data-struture/shared.ts
@@ -0,0 +1,18 @@
+export interface SOAREventI {
+  TicketId: string;
+  SourceSystemName: string;
+  Name: string;
+  DeviceVendor: string;
+  RuleGenerator: string;
+  StartTime: string;
+  Environment: string;
+  Description: string;
+  DisplayId: string;
+  Reason: string;
+  DeviceProduct: string;
+  EndTime: string;
+  Priority: string;
+  EventsList: any;
+  EventProduct: string;
+  EventName: string;
+}
